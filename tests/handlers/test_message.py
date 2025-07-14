1fd0726ab0072ae588b6dd9ca1cffa432952985b
DaX0d <daniil.khodyrev@gmail.com>
2025-07-14 13:27:35 +0300

    test message class added

---
+++ tests/handlers/test_message.py
@@ -0,0 +1,34 @@
+import os
+from dotenv import load_dotenv
+import datetime
+
+from aiogram.types import Message, User, Chat
+
+
+load_dotenv()
+ADMIN_ID = os.getenv('ADMIN_ID')
+
+admin = User(id=int(ADMIN_ID), is_bot=False, first_name='admin')
+user = User(id=1337, is_bot=False, first_name='user')
+
+
+class TestMessage(Message):
+    _current_id = 0
+
+    def __init__(self, text: str, *, user: User = admin):
+        message_id = 52
+        date = datetime.datetime.now()
+        chat = Chat(id=69, type='private')
+        super().__init__(message_id=message_id, date=date, chat=chat, text=text, from_user=user)
+        
+    def answer(self, text, *, parse_mode=None, reply_markup=None) -> dict:
+        return {
+            'text': text,
+            'parse_mode': parse_mode,
+            'reply_markup': reply_markup
+        }  
+
+    @classmethod
+    def __generate_id(cls):
+        cls._current_id = cls._current_id + 1
+        return cls.__current_id
